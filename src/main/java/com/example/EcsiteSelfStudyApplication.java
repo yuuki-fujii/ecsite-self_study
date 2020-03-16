2e333ed4a0bcf471d231d7b2f858a2baf29bbd05
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-03-16 20:32:43 +0900

    first commit

---
+++ src/main/java/com/example/EcsiteSelfStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsiteSelfStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsiteSelfStudyApplication.class, args);
+	}
+
+}
