cf455beb18220a6056f2ed4cf47f14974e82da70
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-03-18 17:39:56 +0900

    合計金額の自動計算機能

--- src/main/resources/static/js/calc_price.js
+++ src/main/resources/static/js/calc_price.js
@@ -5,14 +5,33 @@ $(function(){
 		calc_price();
 	});
 	
+	$("input:checkbox").on("change",function() {
+		calc_price();
+	});
+
+	$("select[name='quantity']").on("change",function() {
+		calc_price();
+	});
 	
 	
 	function calc_price (){
-//		var size = $("input:radio:checked").val();
+		var size = $("input:radio:checked").val();
+		var topping_count = $("input:checkbox:checked").length;
+		var curry_num = $("select[name='quantity'] option:selected").val();
 		
-		console.log(size);
+		if (size == "M") {
+			var size_price = $("#priceSizeM").text().replace(/,/g,'')*1;
+			var topping_price = $("#toppingUnitPriceSizeM").text() * topping_count;
+		} else {
+			var size_price = $("#priceSizeL").text().replace(/,/g,'')*1;
+			var topping_price = $("#toppingUnitPriceSizeL").text() * topping_count;
+		}
 		
+		var price = (size_price + topping_price) * curry_num;
 		
+		console.log(price);
+		
+		$("#totalprice").text(price.toLocaleString());
 		
 	};	
 });
