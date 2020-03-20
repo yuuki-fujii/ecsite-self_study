01a28cde029ca68bae6c3f85ab4e497298804e71
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-03-20 14:58:30 +0900

    ショッピングカートの商品を削除する機能

---
+++ src/main/java/com/example/controller/DeleteFromCartController.java
@@ -0,0 +1,33 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.service.DeleteFromCartService;
+
+/**
+ * カートから商品を削除するコントローラ.
+ * 
+ * @author yuuki
+ *
+ */
+@Controller
+@RequestMapping("/delete_from_cart")
+public class DeleteFromCartController {
+	
+	@Autowired
+	private DeleteFromCartService deleteFromCartService;
+	
+	/**
+	 * 引数の注文商品IDに該当する商品を削除する.
+	 * 
+	 * @param orderItemId 注文商品ID
+	 * @return カート内一覧画面
+	 */
+	@RequestMapping("")
+	public String cartDelete(Integer orderItemId) {
+		deleteFromCartService.cartDelete(orderItemId);
+		return "redirect:/show_cart_list";
+	}
+}
