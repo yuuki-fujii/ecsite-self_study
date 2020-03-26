fae87541b474719d69cfeecd4041e2ccf20b11e2
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-03-26 13:53:42 +0900

    注文確認画面での住所自動取得機能

--- src/main/resources/static/js/get_address.js
+++ src/main/resources/static/js/get_address.js
@@ -2,8 +2,12 @@ $(function(){
 	$('#search_address').on("click",function(){
 		AjaxZip3.zip2addr('zipcode','','address','address');
 	});
-	
-	$('#search_address2').on("click",function(){
+});
+
+
+
+$(function(){
+	$('#search_address_again').on("click",function(){
 		AjaxZip3.zip2addr('destinationZipcode','','destinationAddress','destinationAddress');
 	});
 });
