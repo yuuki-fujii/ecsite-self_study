a6ee021847b659e34a7a6756247a8e73ebae00d6
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-04-27 22:30:10 +0900

    OrderFormにクレジットカード用の項目を追加

--- src/main/java/com/example/form/OrderForm.java
+++ src/main/java/com/example/form/OrderForm.java
@@ -43,6 +43,17 @@ public class OrderForm {
 	private LocalDateTime deliveryTime;
 	/** 支払方法 */
 	private Integer paymentMethod;
+	/** カード番号 */
+	private String card_number;
+	/** 有効期限（年） */
+	private String card_exp_year;
+	/** 有効期限（月） */
+	private String card_exp_month;
+	/** カード名義人 */
+	private String card_name;
+	/** セキュリティコード */
+	private String card_cvv;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -97,7 +108,37 @@ public class OrderForm {
 	public void setStringDeliveryHour(String stringDeliveryHour) {
 		this.stringDeliveryHour = stringDeliveryHour;
 	}
-	
+	public String getCard_number() {
+		return card_number;
+	}
+	public void setCard_number(String card_number) {
+		this.card_number = card_number;
+	}
+	public String getCard_exp_year() {
+		return card_exp_year;
+	}
+	public void setCard_exp_year(String card_exp_year) {
+		this.card_exp_year = card_exp_year;
+	}
+	public String getCard_exp_month() {
+		return card_exp_month;
+	}
+	public void setCard_exp_month(String card_exp_month) {
+		this.card_exp_month = card_exp_month;
+	}
+	public String getCard_name() {
+		return card_name;
+	}
+	public void setCard_name(String card_name) {
+		this.card_name = card_name;
+	}
+	public String getCard_cvv() {
+		return card_cvv;
+	}
+	public void setCard_cvv(String card_cvv) {
+		this.card_cvv = card_cvv;
+	}
+	// フォーム側でdelivaryTimeを加工する
 	public LocalDateTime getDeliveryTime() {
 		try {
 			// stringDeliveryDate と stringDeliveryHourを用いてLocalDateTimeオブジェクトを作成する
@@ -121,13 +162,15 @@ public class OrderForm {
 	public void setPaymentMethod(Integer paymentMethod) {
 		this.paymentMethod = paymentMethod;
 	}
+	
 	@Override
 	public String toString() {
 		return "OrderForm [id=" + id + ", totalPrice=" + totalPrice + ", destinationName=" + destinationName
 				+ ", destinationEmail=" + destinationEmail + ", destinationZipcode=" + destinationZipcode
 				+ ", destinationAddress=" + destinationAddress + ", destinationTel=" + destinationTel
 				+ ", stringDeliveryDate=" + stringDeliveryDate + ", stringDeliveryHour=" + stringDeliveryHour
-				+ ", deliveryTime=" + deliveryTime + ", paymentMethod=" + paymentMethod + "]";
+				+ ", deliveryTime=" + deliveryTime + ", paymentMethod=" + paymentMethod + ", card_number=" + card_number
+				+ ", card_exp_year=" + card_exp_year + ", card_exp_month=" + card_exp_month + ", card_name=" + card_name
+				+ ", card_cvv=" + card_cvv + "]";
 	}
-	
 }
